d8dcf615632d95206edc938873767506d3b65ff9
Philipp Borgers <borgers@mi.fu-berlin.de>
2013-12-17 17:35:17 +0100

    install librarian puppet; install modules; initiate first puppet run

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,3 +4,9 @@ wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
 dpkg -i puppetlabs-release-precise.deb
 aptitude update
 aptitude install puppet-common #standalone mode, no agent foo
+aptitude install rubygems
+gem install librarian-puppet --no-ri --no-rdoc
+cd berlin-puppet/puppet
+librarian-puppet install --verbose
+puppet apply --verbose --modulepath=/root/berlin-puppet/puppet/modules \
+  manifests/site.pp
