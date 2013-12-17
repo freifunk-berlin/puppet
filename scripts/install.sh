392d5527b056dc77521ade90d85713801fca420f
Philipp Borgers <borgers@mi.fu-berlin.de>
2013-12-17 17:37:32 +0100

    download deb to /tmp; remove install.sh if we are done

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,9 @@
 aptitude install git
 git clone https://github.com/freifunk/berlin-puppet.git
+cd /tmp
 wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
 dpkg -i puppetlabs-release-precise.deb
+cd ~
 aptitude update
 aptitude install puppet-common #standalone mode, no agent foo
 aptitude install rubygems
@@ -10,3 +12,5 @@ cd berlin-puppet/puppet
 librarian-puppet install --verbose
 puppet apply --verbose --modulepath=/root/berlin-puppet/puppet/modules \
   manifests/site.pp
+cd ~
+rm install.sh
