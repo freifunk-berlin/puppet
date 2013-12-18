ce06c773172a249850f4e333e942abf396688124
Philipp Borgers <borgers@mi.fu-berlin.de>
2013-12-18 02:34:22 +0100

    first try on php configuration for nginx

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -3,12 +3,6 @@ node 'monitor' {
     ensure => installed,
   }
   class { 'ntp': }
-  class { 'nginx': }
-  nginx::resource::vhost { 'monitor.berlin.freifunk.net':
-    ensure      => present,
-    www_root    => '/srv/www/monitor.berlin.freifunk.net',
-    index_files => ['index.php'],
-  }
   class { '::collectd':
     purge        => true,
     recurse      => true,
@@ -16,6 +10,19 @@ node 'monitor' {
   }
   class { 'collectd::plugin::network':
     listen => $ipaddress,
+  }
+  class { 'nginx': }
+  nginx::resource::vhost { 'monitor.berlin.freifunk.net':
+    ensure      => present,
+    www_root    => '/srv/www/monitor.berlin.freifunk.net',
+    index_files => ['index.php'],
+  }
+  nginx::resource::location { 'php':
+    ensure        => present,
+    location      => '~ [^/]\.php(/|$)',
+    vhost         => 'monitor.berlin.freifunk.net',
+    fastcgi       => 'unix:/var/run/php-fpm-monitor.berlin.freifunk.net.sock',
+
   }
   class { 'php-fpm': }
   php-fpm::pool { 'monitor.berlin.freifunk.net':
