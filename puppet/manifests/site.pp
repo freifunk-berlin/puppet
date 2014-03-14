c871d6624f088ab5edd412831a994d07aa41ed17
Daniel Paufler <dpaufler@leo34.net>
2014-03-14 09:24:14 +0100

    add ipv6 address to collectd

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -28,6 +28,7 @@ node 'monitor' {
   }
   class { 'collectd::plugin::network':
     listen => $ipaddress,
+    listen => $ipaddress6,
   }
   class { 'collectd::plugin::rrdcached':
     daemonaddress => 'unix:/var/run/rrdcached.sock',
