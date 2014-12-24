35e7bf385df318af99402897c3ddc43d24c9a30c
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-12-24 22:37:35 +0100

    add script that creates a dummy key and cert

---
+++ puppet/scripts/ssl.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+COMMON_NAME=$1
+
+if [ "${COMMON_NAME}x" = "x" ]; then
+  echo "Usage: ./genkey.sh <dns>"
+  exit 1
+fi
+
+SUB="
+C=DE
+ST=Berlin
+O=Freifunk Berlin
+localityName=Berlin
+commonName=${COMMON_NAME}
+organizationalUnitName=Freifunk Berlin
+emailAddress=noc@berlin.freifunk.net
+"
+
+openssl req -new \
+  -newkey rsa:4096 -keyout "/etc/ssl/private/${COMMON_NAME}.key" \
+  -sha256 \
+  -nodes \
+  -out "/tmp/${COMMON_NAME}.csr"  \
+  -batch -subj "$(echo -n "$SUB" | tr "\n" "/")"
+
+openssl x509 -req -days 365 -in "/tmp/${COMMON_NAME}.csr" \
+   -signkey "/etc/ssl/private/${COMMON_NAME}.key" \
+   -out "/etc/ssl/certs/${COMMON_NAME}.cert"
