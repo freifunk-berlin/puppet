513e18df2b54a6601fab44d261d8d86f4b635e4b
pmelange <isprotejesvalkata@gmail.com>
2018-10-27 21:28:55 +0200

    add host l105-bbbvpn as b.bbb-vpn.olsr

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -691,3 +691,16 @@ node 'vpn03g' {
   class {'collectd::plugin::processes':}
   class {'collectd::plugin::swap':}
 }
+
+node 'l105-bbbvpn' {
+  class { 'base_node': }
+  class { 'bbbdigger':
+    address          => '77.87.49.11',
+    interface        => 'eth0',
+    mesh_address     => '10.230.38.205',
+    mesh_interface   => 'eth1',
+    tunnel_address   => '10.36.193.1',
+    dhcp_range       => '10.36.193.4,10.36.193.254,255.255.255.0,1h',
+    name             => 'b.bbb-vpn',
+  }
+}
