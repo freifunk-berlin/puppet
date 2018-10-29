50b76e7f040e7cd80002d2c7aa33b5cb841d562c
pmelange <isprotejesvalkata@gmail.com>
2018-10-29 15:40:48 +0100

    change b.bbb-vpn mesh interface to "backbone"

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -698,7 +698,7 @@ node 'l105-bbbvpn' {
     address          => '77.87.49.11',
     interface        => 'eth0',
     mesh_address     => '10.230.38.205',
-    mesh_interface   => 'eth1',
+    mesh_interface   => 'backbone',
     tunnel_address   => '10.36.193.1/24',
     dhcp_range       => '10.36.193.4,10.36.193.254,255.255.255.0,1h',
     name             => 'b.bbb-vpn',
