e90393ccc785eb5f6f5a1296d8cd897132eea688
Nicolas Berens <nicolas.berens@betrolltes-wohnen.de>
2020-03-13 12:58:02 +0100

    also generate dh params

--- puppet/scripts/ssl.sh
+++ puppet/scripts/ssl.sh
@@ -8,9 +8,8 @@ if [ "${COMMON_NAME}x" = "x" ]; then
 fi
 
 if [ -f "/etc/ssl/private/${COMMON_NAME}.key" ]; then
-  echo "Cert already exists, skipping generating it"
-  exit 0
-fi
+    echo "Cert already exists, skipping generating it"
+  else
 
 SUB="
 C=DE
@@ -32,3 +31,12 @@ openssl req -new \
 openssl x509 -req -days 365 -in "/tmp/${COMMON_NAME}.csr" \
    -signkey "/etc/ssl/private/${COMMON_NAME}.key" \
    -out "/etc/ssl/certs/${COMMON_NAME}.cert"
+
+fi
+
+if [ -f "/etc/ssl/private/${COMMON_NAME}.dh" ]; then
+    echo "DH Param already exists, skipping generating it"
+  else
+    # Only 1024 Bit to make the Box Provisioning faster
+    openssl dhparam -out "/etc/ssl/private/${COMMON_NAME}.dh" 1024
+fi
